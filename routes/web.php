08dfca21e5befee26e8c83436a690fed7485d99a
Sifuhrey <145308339+Sifuhrey@users.noreply.github.com>
2024-11-10 20:34:11 +0700

    regis

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [ProductController::class, 'index'])->name('product.index');
 Route::post('/', [ProductController::class, 'store'])->name('product.store');
+Route::get('/regis', [UserController::class, 'regis']);
+Route::post('/regis', [UserController::class, 'store'])->name('store');
+
 

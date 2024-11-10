6758311992fc71454123bbdcc16cd51eba21e745
Definats <defina.trisalsabila05@gmail.com>
2024-11-10 21:27:48 +0700

    yes

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class userController extends Controller
+class UserController extends Controller
 {
+    public function regis(){
+      return view('register');
+    }
     public function index()
     {
 
         return view(
             'login'
-        );
-    }
+    );
+  }
     public function store(Request $request){
-
+      
     }
 }

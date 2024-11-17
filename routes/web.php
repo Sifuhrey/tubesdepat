7081fc68943c0ffe8589b23966be06161adb7512
fitriyanuralyashifa <fitriyaalya24@gmail.com>
2024-11-17 14:38:21 +0700

    bug

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', [ProductController::class, 'index'])->name('product.index');
 Route::post('/', [ProductController::class, 'store'])->name('product.store');
-Route::get('/login', [UserController::class, 'index'])->name('product.store');
+Route::get('/login', [UserController::class, 'login'])->name('login');
 Route::get('/regis', [UserController::class, 'regis']);
 Route::post('/regis', [UserController::class, 'store'])->name('store');
 

e8658ac3c75125bb4dd484bb33b113aaca5dedc3
fitriyanuralyashifa <fitriyaalya24@gmail.com>
2024-11-17 14:51:12 +0700

    ss

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
-class userController extends Controller
+class UserController extends Controller
 {
     public function regis(){
       return view('register');
